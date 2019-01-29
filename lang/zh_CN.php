360f14b04b91245a0e46f820b9a964427488c9e9
kdrealm <btmouse@gmail.com>
2019-01-29 16:22:43 +0800

    Create zh_CN.php

---
+++ lang/zh_CN.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * Simplified Chinese
+ */
+
+return array(
+
+	// Header
+	'Network Traffic' => '流量统计',
+	'All Interfaces' => '所有接口',
+
+	// Tabs
+	'Summary' => '概览',
+	'Hourly' => '每小时',
+	'Daily' => '每天',
+	'Weekly' => '每星期',
+	'Monthly' => '每月',
+	'Top 10' => '前十',
+	'Reports' => 'Relatórios',
+	
+	// Summary
+	'Today' => '今天',
+	'Yesterday' => '昨天',
+	'Information' => '信息',
+	'This Month' => '这个月',
+	'Last Month' => '上个月',
+	'All Time' => '所有时间',
+	'Since' => '自',
+	'Updated' => '更新时间',
+	
+	// Tables
+	'Hour' => '时间',
+	'Day' => '日期',
+	'Week' => '星期',
+	'Month' => '月份',
+	'Average' => '平均',
+	'Received' => '下载',
+	'Sent' => '上传',
+	'Total' => '总计',
+	
+);
